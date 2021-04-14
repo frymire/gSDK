f37993a7136027bac30413916e2e5400d5ad0177
Mark Frymire <mark.e.frymire@wbbinc.com>
2021-04-14 12:21:48 -0400

    Turn off follow mode for pointing commands

--- mavlink_control.cpp
+++ mavlink_control.cpp
@@ -385,12 +385,12 @@ void PointIncremental(Gimbal_Interface &gimbal, float yaw, float pitch, float ro
 
 void PointAbsolute(Gimbal_Interface &gimbal, float yaw, float pitch, float roll) {
   printf("PointAbsolute YPR = [%f, %f, %f]...\n", yaw, pitch, roll);
-  SetFollowMode(gimbal);
+  //SetFollowMode(gimbal);
   mavlink_mount_orientation_t mnt_orien = gimbal.get_gimbal_mount_orientation();
   gimbal.reset_acks();
   gimbal.set_gimbal_move(-1*mnt_orien.pitch + pitch, mnt_orien.roll + roll, mnt_orien.yaw + yaw);
   WaitForCommandAck(gimbal, 50000);
-  SetLockMode(gimbal);
+  //SetLockMode(gimbal);
   printf("PointAbsolute complete.\n");
 }
 

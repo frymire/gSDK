bb7c476c2778d29bdaa77697c391bf23e707f9ad
Mark Frymire <mark.e.frymire@wbbinc.com>
2021-04-14 16:25:58 -0400

    Debug reading file on Linux

--- mavlink_control.cpp
+++ mavlink_control.cpp
@@ -99,11 +99,20 @@ int main(int argc, char** argv) {
     PointingCommand commands[k_num_timesteps];
     int j = 0;
     int temp_active;
+
+    char* line = NULL;
+    size_t len = 0;
+
     while(j < k_num_timesteps) {
+
+      ssize_t read = getline(&line, &len, p_file);
+      printf("Retrieved line of length %zu:\n", read);
+      printf("%s", line);
+
     //while(!feof(p_file)) {
-      fscanf(p_file, "%d,%d,%f,%f,%f", &commands[j].index, &temp_active, &commands[j].yaw, &commands[j].pitch, &commands[j].roll);
-      if(temp_active) { commands[j].active = true; } else { commands[j].active = false; }
-      printf("command = %d %d %f %f %f\n", commands[j].index, commands[j].active, commands[j].yaw, commands[j].pitch, commands[j].roll);
+      //fscanf(p_file, "%d,%d,%f,%f,%f", &commands[j].index, &temp_active, &commands[j].yaw, &commands[j].pitch, &commands[j].roll);
+      //if(temp_active) { commands[j].active = true; } else { commands[j].active = false; }
+      //printf("command = %d %d %f %f %f\n", commands[j].index, commands[j].active, commands[j].yaw, commands[j].pitch, commands[j].roll);
       j++;
     }
 
